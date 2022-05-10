15d84a5a6ae72d68d38ebc18f2e4488a34a5af31
AntoineB <bodein.antoine@gmail.com>
2022-05-10 10:51:17 -0400

    Update with liver v1

--- R/export_to_cytoscape.R
+++ R/export_to_cytoscape.R
@@ -1,6 +1,85 @@
 #' @importFrom NetPathMiner plotCytoscapeGML
 #' @export
-export_to_cytoscape <- function(x, file){
+export_to_cytoscape <- function(x, collectionName = "myNewCollection", networkName = "myNetwork"){
+
+    # check x
+    stopifnot(is(x, "get_route.res"))
+
+    checkmate::assert_string(collectionName)
+    checkmate::assert_string(networkName)
+
+    # RCy3
+    RCy3::cytoscapePing()  # stop execution if status != 200
+
+    collectionList.names <- RCy3::getCollectionList()
+    if(!(collectionName %in% collectionList.names)){
+        message(paste0("The collection ", collectionName, " does not exist and will be created."))
+    }
+    networkList.names <- RCy3::getNetworkList()
+    if(networkName %in% networkList.names){
+        paste0("The network ", networkName, " already exists and will be overwritten.")
+    }
+
+
+    # create network
+    RCy3::createNetworkFromIgraph(igraph = x$network, title = networkName,
+                                  collection = collectionName)
+    # from dataframes
+    # edge_node_list <- convert_get_route_to_data.frames(x)
+    # RCy3::createNetworkFromDataFrames(edge_node_list$node,edge_node_list$edge,
+    #                                   title=networkName, collection = collectionName)
+
+
+    # build and set visual style
+    build_visual_style(style.name = "cosmEU_Style", network = networkName)
+    RCy3::setVisualStyle("cosmEU_Style", network = networkName)
+    # hideEdges()
+}
+
+#' build visual style
+build_visual_style <- function(style.name = "cosmEU_Style", network){
+
+    defaults <- list(NODE_SHAPE="ROUND_RECTANGLE",
+                     NODE_SIZE=30,
+                     EDGE_TRANSPARENCY=120,
+                     NODE_LABEL_POSITION="C,C,c,0.00,0.00")
+
+    nodeLabels <- RCy3::mapVisualProperty('node label','id','p')
+
+    # fill color
+    fill_color <- tribble(
+        ~type, ~hex,
+      "ENSEMBL", "#96cbff",
+      "UNIPROT", "#FFA500",
+        "SMPDB", "#f77272",
+     "DrugBank", "#fcc2f6",
+       "CHEMBL", "#00FF00")
+
+    nodeFills <- RCy3::mapVisualProperty(visual.prop = 'node fill color',
+                                   table.column = 'type',
+                                   mapping.type = 'd',
+                                   table.column.values = fill_color$type,
+                                   visual.prop.values = fill_color$hex,
+                                   network = network)
+
+    nodeLabels <- RCy3::mapVisualProperty('node label','display_name','p')
+    # edgeLineStyle <- mapVisualProperty('EDGE_LINE_TYPE', table.column = "interaction",
+    #                                    mapping.type = 'd',
+    #                                    table.column.values = c("interacts with","closest"),
+    #                                    visual.prop.values = c("SOLID", "DOTS"),
+    #                                    network = network)
+
+    # arrowShapes <- mapVisualProperty('Edge Target Arrow Shape','interaction','d',
+    #                                  c("activates","inhibits","interacts"),c("Arrow","T","None"))
+    # edgeWidth <- mapVisualProperty('edge width','weight','p')
+
+    # and then create the style
+    RCy3::createVisualStyle(style.name, defaults, list(nodeFills, nodeLabels))
+    # createVisualStyle(style.name, defaults, list(nodeFills, nodeLabels, edgeLineStyle))
+}
+
+#' @importFrom NetPathMiner plotCytoscapeGML
+export_to_cytoscape_w_netPathMiner <- function(x, file, collectionName = "myNewCollection", networkName = "myNetwork"){
 
     # check x
     stopifnot(is(x, "igraph"))
@@ -9,6 +88,33 @@ export_to_cytoscape <- function(x, file){
     stopifnot(checkmate::checkPathForOutput(file))
 
     # check layout
-
     NetPathMiner::plotCytoscapeGML(x, file=file)
 }
+
+convert_igraph_to_dataframes <- function(x){
+    Vx <- vertex_attr(x) %>% as.data.frame()
+    Ex <- x %>% as_long_data_frame() %>% dplyr::select(from_name, to_name, from_type, to_type) %>%
+        mutate(interaction = "interacts with")
+
+    return(list(V = Vx, E = Ex))
+}
+
+convert_get_route_to_data.frames <- function(x){
+    tmp <- convert_igraph_to_dataframes(x$network)
+
+    join.df <- as.data.frame(tmp$V) %>% dplyr::select(name, type)
+
+    Ex_closest <- x$closest %>% dplyr::select(SeedName, NodeNames) %>%
+        left_join(join.df, c("SeedName" = "name")) %>%
+        dplyr::rename("from_type" = "type") %>%
+        left_join(join.df, c("NodeNames" = "name")) %>%
+        purrr::set_names("from_name", "to_name", "from_type", "to_type") %>%
+        mutate(interaction = "closest")
+
+    new_E <- rbind(tmp$E, Ex_closest) %>%
+        purrr::set_names("source", "target", "from_type", "to_type", "interaction")
+    new_V <- tmp$V %>% mutate(id = name)
+
+    return(list(node = tmp$V, edge = new_E))
+}
+
--- R/get_route.R
+++ R/get_route.R
@@ -2,36 +2,55 @@
 #'
 #' @importFrom netOmics random_walk_restart
 #' @importFrom netOmics rwr_find_closest_type
-#' @importFrom igraph induced_subgraph
+#' @importFrom igraph induced_subgraph shortest_paths
 #' @importFrom purrr map2
 #' @export
+
 get_route <- function(network, signature, input_type = c("ENSEMBL"),
-                      target_type = c("UNIPROT", "SMPDB", "DrugBank","CHEMBL")){
+                      target_type = c("UNIPROT", "SMPDB", "DrugBank","CHEMBL"),
+                      num_cores = 4){
+
+    # check if num_cores ok
 
     # check if signature is not empty
-    stopifnot(is_empty(signature))
+    stopifnot(!purrr::is_empty(signature))
+
 
     #1 -- get RWR
+    print("get RW scores")
     RWR_res <- netOmics::random_walk_restart(X = network, seed = signature)
     # can be computed
     ## /!\ Cholmod error 'problem too large' at file ../Core/cholmod_dense.c, line 102
     ## works with no PPI ego
 
-    closest <- lapply(target_type,
-                      function(x){netOmics::rwr_find_closest_type(X = RWR_res,
-                                                                  seed = signature,
-                                                                  attribute = "type",
-                                                                  value = x) %>%
-            map_dfr(~.x %>% as.data.frame)}) %>%
-        map_dfr(~.x)
+    # SP_res <- shortest_paths(graph = network, from = signature, to = test_target)
+    print("find closest")
+    closest.dfr <- parallel::mclapply(target_type, function(x){
+        netOmics::rwr_find_closest_type(X = RWR_res,
+                                        seed = signature,
+                                        attribute = "type",
+                                        value = x) %>%
+            map_dfr(~.x %>% as.data.frame)}, mc.cores = num_cores)
 
+    closest <- map_dfr(closest.dfr, ~.x)
+
+    # closest <- netOmics::rwr_find_closest_type(X = RWR_res, seed = signature, attribute = "type", value = target_type[[i]])
     # get SP
     input <- closest$SeedName
     target <- closest$NodeNames
 
-    all_sp <-  purrr::map2(input, target, ~{shortest_paths(graph=network, from=.x, to=.y) %>% .$vpath %>% lapply(names) %>% unlist()})
-    sub.res <- igraph::induced_subgraph(network, unlist(all_sp))
+    print("extract shortest path")
+    all_sp <-  map2(input, target, ~{igraph::shortest_paths(graph=network, from=.x, to=.y) %>% .$vpath %>% lapply(names) %>% unlist()})
+    # SP <- shortest_paths(graph = network, from = closest$SeedName[1], to = closest$NodeNames[1])
+    # igraph::induced_subgraph(network, SP$vpath[[1]]) %>% plot
+    #
+    induced.g <- igraph::induced_subgraph(network, unlist(all_sp))
+    vertex_attr(induced.g) <- vertex_attr(induced.g) %>% as.data.frame() %>%
+        mutate(input_signature = name %in% signature)
 
-    return(sub.res)
+
+    result <- list(network = induced.g, closest = closest)
+    class(result) <- "get_route.res"
+    return(result)
+
 }
-
