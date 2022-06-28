d35693e5e66710a311d1dad9052bda2dfb2f6054
AntoineB <bodein.antoine@gmail.com>
2022-06-27 20:51:51 -0400

    update tests

--- tests/testthat/test-get_route.R
+++ tests/testthat/test-get_route.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# get route
+data(liver_1.3_network)
+data(liver_1.3_rwr_closest_dfr)
+data(signature_maison)
+
+signature_vids <- signature_maison$acetaminophen_all_all
+res_diffusion <- get_route(liver_1.3_network, liver_1.3_rwr_closest_dfr, signature_vids)
+res_report <- report(res_diffusion)
+
+test_that("get_route works", {
+    expect_s3_class(res_diffusion, "get_route.res")
+})
+
+test_that("report works", {
+    expect_type(res_report, "list")
+    expect_equal(length(res_report), 9)
 })
